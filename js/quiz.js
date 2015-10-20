af1bee518d072a2f990dbd091b88ee338cbf82c1
Aniruddh Iyengar <aniruddh.iyengar202@gmail.com>
2015-10-20 09:08:17 -0400

    Migrated Jaro-Winkler to separate file

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,11 @@
 var ipc = require('ipc');
 var list = [];
 
-// JARO-WINKLER STRING DISTANCE
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('n=g(5,6){8 f=5.h,c=6.h,e=t.s(f,c)/2-1,a=0,7=0,9=\'\';q(8 i u 6){9=6[i];b(5.j(i,i+e).k(9)>-1){7++}o b(5.j(i-e,i).k(9)>-1){7++;a++}};r(1/3*(7/f+7/c+(7-a)/7))};w=g(5,6,p){5=5.m();6=6.m();p=p||0.1;8 d=n(5,6),l=0;q(8 i=0;i<4;i++){b(5[i]==6[i]){l++}o{v}}r d+(l*p*(1-d))};',33,33,'|||||str1|str2|matches|var|letter|transpositions|if|lenStr2|dj|matchWindow|lenStr1|function|length||slice|indexOf||toLowerCase|jaro|else||for|return|max|Math|in|break|jaroWinkler'.split('|'),0,{}));
-
 var resizeElements = function() {
 	$("#quiz").css("top", $(window).height()/2 - 150 + "px");
 }
 
-var game, done, currQ, currA, numRight, numWrong, currPos, table;
+var game = false, done, currQ, currA, numRight, numWrong, currPos, table;
 
 var AssocTable = function(keyvals) {
 	this.vals = keyvals;
@@ -49,22 +46,18 @@ ipc.on('notes-clean', function(arg) {
 		}
 	}
 	list = l;
-
-	if (!game) {
-		game = true;
-		$("#submit").text("Submit");
-		$("#ans").removeAttr("disabled").focus();
-		game = false;
-		done = false;
-		currQ = "";
-		currA = "";
-		numRight = 0;
-		numWrong = 0;
-		currPos = 0;
-		table = new AssocTable(list);
-		populate();
-	}
-	else verify();
+	game = true;
+	$("#submit").text("Submit");
+	$("#ans").removeAttr("disabled").focus();
+	game = true;
+	done = false;
+	currQ = "";
+	currA = "";
+	numRight = 0;
+	numWrong = 0;
+	currPos = 0;
+	table = new AssocTable(list);
+	populate();
 });
 
 $(window).resize(resizeElements);
@@ -84,6 +77,8 @@ var populate = function() {
 		$("#question p.q").text(currQ);
 		$("#ans").val("");
 	}
+
+	$("#ans").focus();
 
 }
 
@@ -117,13 +112,19 @@ var verify = function() {
 }
 
 $("#submit").click(function() {
-
-	ipc.send('note', 'get');
+	console.log(game);
+	if (!game) {
+		ipc.send('note', 'get');
+	}
+	else {
+		verify();
+	}
 
 });
 
 document.onkeydown = function(evt) {
 	if ($(":focus").attr("id") == "ans" && evt.keyCode == 13) {
+		console.log(game);
 		verify();
 	}
 }
