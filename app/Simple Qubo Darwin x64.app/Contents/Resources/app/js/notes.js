538ccdf876a257ae666b9e860060b7f90e31710d
Aniruddh Iyengar <aniruddh.iyengar202@gmail.com>
2016-04-28 14:11:13 -0400

    added darwin app

---
+++ app/Simple Qubo Darwin x64.app/Contents/Resources/app/js/notes.js
@@ -0,0 +1,187 @@
+var ipc = require("ipc");
+var notes = [];
+var files = [];
+var currFile = '';
+
+ipc.send('files');
+
+ipc.on('files-trigger', function(arg) {
+	ipc.send('files');
+});
+
+var w = $("#sidebar").width() + 2 * 30;
+
+var resizeElements = function() {
+	$("#main-container").width($(window).width() - 400);
+	$("#main").height($(window).height() - ($("#search-bar").height() + 40));
+	$(".note-right").width($("#main").width() - 248);
+};
+
+var loadFiles = function() {
+	$("#files-list").html("");
+	for (var i = 0; i < files.length; i++) {
+		var e1 = $("<option></option>").attr("data-file", "notes-" + files[i] + ".json");
+		$(e1).text(files[i]);
+		$("#files-list").append(e1);
+	}
+	
+	if (currFile != null) {
+		currFile = files[0];
+	}
+	$("#files-list").val(currFile);
+	$("#filename").val(currFile);
+	ipc.send('file', {
+		file: currFile
+	});
+}
+
+var load = function() {
+	$("#main").html("");
+	for (var i = 0; i < notes.length; i++) {
+		var element = $("<div class='note'></div>").attr('data-id', notes[i].id);
+		$(element).append($("<div class='note-left'></div>")
+			.text(notes[i].answer).append($("<div class='x'></div>")));
+		var clues = notes[i].clues.split('\n');
+		var noteRight = $("<div class='note-right'></div>");
+		var noteList = $("<ul class='note-list'></ul>").attr('data-id', notes[i].id);
+		for (var j = 0; j < clues.length; j++) {
+			$(noteList).append($("<li></li>").append(clues[j]).append($("<div class='x'></div>")));
+		}
+		$(noteRight).append($(noteList));
+		$(element).append($(noteRight));
+		$("#main").append($(element));
+	}
+
+	$(".note-left").hover(function() {
+		$(this).children(".x").fadeTo(0, 1);
+	}, function() {
+		$(this).children(".x").fadeTo(0, 0);
+	});
+
+	$(".note-list li").hover(function() {
+		$(this).children(".x").fadeTo(0, 1);
+	}, function() {
+		$(this).children(".x").fadeTo(0, 0);
+	});
+
+	$(".x").click(function() {
+		if ($(this).parent().hasClass("note-left")) {
+			// remove whole note
+			var id = $(this).parent().parent().data("id");
+			ipc.send('remove-note', id);
+		}
+		else {
+			// remove single clue
+			var id = $(this).parent().parent().data("id");
+			var ix = $(this).parent().index();
+			ipc.send('remove-clue', {id: id, ix: ix});
+		}
+	});
+
+	$(".note-left").dblclick(function() {
+		$("#answer").val($(this).text());
+		$("#clues").focus();
+	});
+
+	resizeElements();
+};
+
+$(window).resize(resizeElements);
+resizeElements();
+
+var submit = function() {
+	var answer = $.trim($("#answer").focus().val());
+	var clues = $.trim($("#clues").val());
+	$("#answer, #clues").val("");
+	ipc.send('note', {
+		answer: answer,
+		clues: clues
+	});
+};
+
+$("#submit").click(submit);
+
+ipc.on('files-list', function(arg) {
+	files = arg;
+	loadFiles();
+});
+
+ipc.on('notes-clean', function(arg) {
+	notes = arg;
+	load();
+});
+
+ipc.on('file-loaded', function(arg) {
+	currFile = arg.filename;
+	$("#filename").val(arg.filename);
+	$("#files-list").val(arg.filename);
+	notes = arg.notes;
+	load();
+});
+
+$("#files-list").change(function() {
+	currFile = $(this).val();
+	ipc.send('file', {
+		file: $(this).val()
+	});
+});
+
+$("#filename").change(function() {
+	currFile = $(this).val();
+	ipc.send('rename', $(this).val());
+});
+
+$("#search-box").keyup(function() {
+	var list = $(".note");
+	if ($(this).val().trim().length > 2) {
+		for (var i = 0; i < list.length; i++) {
+			var el = $(list[i]);
+			var t = $(el).children(".note-left").text();
+			var clues = $(el).children('.note-right').children(".note-list").children("li");
+			for (var j = 0; j < clues.length; j++) {
+				t = t + " " + $(clues[j]).text();
+			}
+			t = t.toLowerCase();
+			var term = $(this).val().trim().toLowerCase();
+			if (t.indexOf(term) > -1) {
+				$(el).show();
+			}
+			else {
+				$(el).hide();
+			}
+		}
+	}
+	else {
+		$(".note").show();
+	}
+});
+
+$("#take-quiz").click(function() {
+	ipc.send('start-quiz');
+});
+
+$("#newfile").click(function() {
+	ipc.send('new-file');
+});
+
+document.onkeydown = function(evt) {
+	if (!evt) evt = event;
+	if (evt.shiftKey && evt.keyCode == 13) {
+		submit();
+		$("#answer").focus();
+		evt.preventDefault();
+	} 
+	if (evt.keyCode == 13) {
+		if ($(":focus").attr("id") == "answer" && $("#answer").val().trim().length != 0) {
+			$("#clues").focus();
+			evt.preventDefault();
+		}
+		else if ($(":focus").attr("id") == "search-box" && $("#search-box").val().trim().length > 2) {
+			$("#search-box").trigger('enter');
+		}
+	}
+
+};
+
+
+
---
+++ app/Simple Qubo Darwin x64.app/Contents/Resources/app/main.js
@@ -0,0 +1,192 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var ipc = require('electron').ipcMain;
+var fs = require('fs');
+
+//var filename = __dirname + '/notes.json';
+var notesDir = __dirname + '/notes/';
+var currFile = "";
+var files = [];
+var notes = [];
+
+require('crash-reporter').start();
+
+var notesWindow = null;
+var quizWindow = null;
+
+var write = function() {
+	fs.writeFileSync(notesDir + currFile + '.json', JSON.stringify(notes));
+}
+
+var process = function(note) {
+
+	if (note.answer.trim().length == 0 || note.clues.trim().length == 0) {
+		return;
+	}
+	else {
+		for (var i = 0; i < notes.length; i ++) {
+			if (notes[i].answer.toLowerCase() == note.answer.toLowerCase()) {
+				note.clues = notes[i].clues + '\n' + note.clues;
+				note.answer = notes[i].answer;
+				notes.splice(i--, 1);
+			}
+		}
+
+		note.id = '' + Date.now();
+
+		notes.unshift(note);
+	}
+
+}
+
+var loadFile = function(fname, sender) {
+	currFile = fname;
+	console.log(currFile);
+	var text = fs.readFileSync(notesDir + fname + '.json');
+	notes = JSON.parse(text);
+	sender.send('file-loaded', {
+		filename: fname,
+		notes: notes
+	});
+};
+
+var loadFileCombined = function(arg, sender) {
+	var fileList = arg.files;
+	var fullNotes = [];
+	for (var i = 0; i < fileList.length; i++) {
+		var t = fs.readFileSync(notesDir + fileList[i] + '.json');
+		fullNotes = fullNotes.concat(JSON.parse(t));
+	}
+	sender.send('notes-clean', fullNotes);
+}
+
+app.on('window-all-closed', function() {
+	app.quit();
+});
+
+ipc.on('files', function(evt, arg) {
+	fs.readdir(notesDir, function(err, f) {
+		if (err) throw err;
+		files = [];
+
+		for (var k = 0; k < f.length; k++) {
+			if (f[k] == '.DS_Store' || f[k] == '.gitignore') continue;
+			else {
+				var fn = f[k];
+				files.push(fn.slice(0, -5));
+			}
+		}
+
+		evt.sender.send('files-list', files);
+
+	});
+});
+
+ipc.on('note-combined', function(evt, arg) {
+	loadFileCombined({
+		files: arg.files
+	}, evt.sender);
+});
+
+ipc.on('rename', function(evt, arg) {
+	fs.renameSync(notesDir + currFile + '.json', notesDir + arg + '.json');
+	currFile = arg;
+
+	fs.readdir(notesDir, function(err, f) {
+		if (err) throw err;
+		files = [];
+
+		for (var k = 0; k < f.length; k++) {
+			if (f[k] == '.DS_Store' || f[k] == '.gitignore') continue;
+			else {
+				var fn = f[k];
+				files.push(fn.slice(0, -5));
+			}
+		}
+
+		evt.sender.send('files-list', files);
+		loadFile(currFile, evt.sender);
+
+	});
+});
+
+ipc.on('file', function(evt, arg) {
+	loadFile(arg.file, evt.sender);
+});
+
+ipc.on('note', function(evt, arg) {
+	if (arg.type == 'get') {
+		evt.sender.send('notes-clean', notes);
+	}
+	else {
+		process(arg);
+		write();
+		evt.sender.send('notes-clean', notes);
+	}
+});
+
+ipc.on('remove-note', function(evt, arg) {
+	for (var i =0; i < notes.length; i++) {
+		if (notes[i].id == arg) {
+			notes.splice(i, 1);
+			break;
+		}
+	}
+	write();
+	evt.sender.send('notes-clean', notes);
+});
+
+ipc.on('remove-clue', function(evt, arg) {
+	for (var i = 0; i < notes.length; i++) {
+		if (notes[i].id == arg.id) {
+			var a = notes[i].clues.split('\n');
+			a.splice(arg.ix, 1);
+			if (a.length == 0) {
+				notes.splice(i, 1);
+			}
+			else {
+				notes[i].clues = a.join('\n');
+			}
+			break;
+		}
+	}
+	write();
+	evt.sender.send('notes-clean', notes);
+});
+
+ipc.on('start-quiz', function(evt, arg) {
+	quizWindow = new BrowserWindow({width: 1000, height: 600});
+	quizWindow.loadURL('file://' + __dirname + '/views/quiz.html');
+});
+
+ipc.on('new-file', function(evt, arg) {
+	currFile = 'Untitled-' + Math.floor(new Date());
+	fs.writeFileSync(notesDir + currFile + '.json', '[]');
+
+	fs.readdir(notesDir, function(err, f) {
+		if (err) throw err;
+		files = [];
+
+		for (var k = 0; k < f.length; k++) {
+			if (f[k] == '.DS_Store' || f[k] == '.gitignore') continue;
+			else {
+				var fn = f[k];
+				files.push(fn.slice(0, -5));
+			}
+		}
+
+		evt.sender.send('files-list', files);
+		loadFile(currFile, evt.sender);
+
+	});
+});
+
+app.on('ready', function() {
+	notesWindow = new BrowserWindow({width: 1000, height: 600});
+	notesWindow.loadURL('file://' + __dirname + '/views/notes.html');
+
+	notesWindow.on('closed', function() {
+		notesWindow = null;
+	});
+});
+
